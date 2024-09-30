7b2cec3bd61c8cdf7e85ebffb73e1c59ace36301
mayank <mayankrajput40558@gmail.com>
2024-09-30 14:12:18 +0530

    done

--- program2.py
+++ program2.py
@@ -1,10 +1,22 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+         # If the input string is empty, return 0
+        if not s:
+            return 0
+        res = 0
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
-
-
+        for a, b in zip(s, s[1:]):
+            if roman[a] < roman[b]:
+                res -= roman[a]
+            else:
+                res += roman[a]
+        return res + roman[s[-1]]
